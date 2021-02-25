7d54a1d649c9ac9c7bb60a451bb5b96555978287
Kris Sakarias <ksakarias@gmail.com>
2021-02-24 18:25:20 -0800

    initial boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Scaffold
+
+## What is React Scaffold?
---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <p>React Scaffold Loaded...</p>
+    </div>
+  );
+};
+
+export default App;
